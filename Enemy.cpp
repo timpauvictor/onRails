58c6a0cc768c77cdd82215f70c0f5a474ea2637b
Carlos <carlossantana3279@gmail.com>
2016-12-09 16:37:57 -0500

    Enemies scale with radius only

--- Enemy.cpp
+++ Enemy.cpp
@@ -33,20 +33,20 @@ Description - On rails shooter game created with c++ and openGL. This is the mai
 Enemy Constructor
 x,y,z, enemy radius, scale (** Keep at 0.2), waitTime, popup Direction, movement speed
 */
-Enemy::Enemy(float xcord, float ycord, float zcord,float rad, float theScale, float initTime,float popDir, float speed){	//constructor
-	//store the center of the cube
-	x = xcord;
-	y = ycord;
-	z = zcord;
+Enemy::Enemy(float xcord, float ycord, float zcord,float rad, float theScale, float initTime,float popDir, float speed){  //constructor
+  //store the center of the cube
+  x = xcord;
+  y = ycord;
+  z = zcord;
   //enemy rotation
-	rotX = 0;
-	rotY = 0;
-	rotZ = 0;
-	active = false;           //enemies start off in active
+  rotX = 0;
+  rotY = 0;
+  rotZ = 0;
+  active = false;           //enemies start off in active
   moved = false;            //for when the enemy has finished poping up
 
-	scale = theScale;
-  radius = 5.8*rad*scale;   //the new radius
+  scale = theScale;
+  radius = rad;   //the new radius
 
   point3D p(x,y,z);
   point3D p1(x+1,y,z);
@@ -153,8 +153,8 @@ void Enemy::draw(float camPos0, float camPos1, float camPos2){
 
       //Used for testing      
       //hit Box
-      //glColor3f(0,1,0);
-      //glutWireSphere(radius,50,10);
+      glColor3f(0,1,0);
+      glutWireSphere(radius,50,10);
       
       //Materials      
       float m_amb[] = {0.2, 0.2, 0.2, 1.0};
@@ -162,31 +162,7 @@ void Enemy::draw(float camPos0, float camPos1, float camPos2){
       float m_spec[] = {0.04, 0.7, 0.7, 1.0};
       float shiny = 0.078125 *128;
 
-      //Obsidian Material
-      m_amb[0] = 0.05375;
-      m_amb[1] = 0.05;
-      m_amb[2] = 0.06625;
-      m_amb[3] = 0.82;
-      m_dif[0] = 0.18275;
-      m_dif[1] = 0.17;
-      m_dif[2]= 0.22525;
-      m_dif[3] = 0.82;
-      m_spec[0] = 0.332741;
-      m_spec[1] = 0.328634;
-      m_spec[2]= 0.508273; 
-      m_spec[3] = 0.346435;
-      shiny = 38.4;
-      glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, m_amb);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, m_dif);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, m_spec);
-      glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shiny);
 
-      //Main Gun
-      glPushMatrix();
-      //Torus
-        glTranslatef(0,0,1);
-        glutSolidTorus(0.2,0.4,50,50);
-      glPopMatrix();
       
       //Chrome Material
       m_amb[0] = 0.25;
@@ -206,66 +182,95 @@ void Enemy::draw(float camPos0, float camPos1, float camPos2){
 
       //Body
       glPushMatrix();
-        glutSolidSphere(1.2, 10, 50);
+        glutSolidSphere(radius, 10, 50);
+        //RED Material
+        m_amb[0] = 0.1745;
+        m_amb[1] = 0.01175;
+        m_amb[2] = 0.01175;
+        m_dif[0] = 0.61424;
+        m_dif[1] = 0.04316;
+        m_dif[2]= 0.04136;
+        m_spec[0] = 0.727811;
+        m_spec[1] = 0.626959;
+        m_spec[2]= 0.626959; 
+        shiny = 0.6 * 128;
+        glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, m_amb);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, m_dif);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, m_spec);
+        glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shiny);
+
+        //Center Shooting Part
+        glPushMatrix();
+          glTranslatef(0,0,-1*radius+0.3*radius);
+          glColor3f(1,0,0);
+          glutSolidSphere(0.4*radius,10,100);
+        glPopMatrix();
+
+
+        //Obsidian Material
+        m_amb[0] = 0.05375;
+        m_amb[1] = 0.05;
+        m_amb[2] = 0.06625;
+        m_amb[3] = 0.82;
+        m_dif[0] = 0.18275;
+        m_dif[1] = 0.17;
+        m_dif[2]= 0.22525;
+        m_dif[3] = 0.82;
+        m_spec[0] = 0.332741;
+        m_spec[1] = 0.328634;
+        m_spec[2]= 0.508273; 
+        m_spec[3] = 0.346435;
+        shiny = 38.4;
+        glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, m_amb);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, m_dif);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, m_spec);
+        glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shiny);
+
+        //Main Gun
+        glPushMatrix();
+        //Torus
+          glTranslatef(0,0,-1*radius + 0.2*radius );
+          glutSolidTorus(0.7*radius/5,0.7*radius/5 + 0.2*radius,50,50);
+        glPopMatrix();
+
+
+        //Obsidian Material
+        m_amb[0] = 0.05375;
+        m_amb[1] = 0.05;
+        m_amb[2] = 0.06625;
+        m_amb[3] = 0.82;
+        m_dif[0] = 0.18275;
+        m_dif[1] = 0.17;
+        m_dif[2]= 0.22525;
+        m_dif[3] = 0.82;
+        m_spec[0] = 0.332741;
+        m_spec[1] = 0.328634;
+        m_spec[2]= 0.508273; 
+        m_spec[3] = 0.346435;
+        shiny = 38.4;
+        glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, m_amb);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, m_dif);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, m_spec);
+        glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shiny);
+
+        //left shoulder Shooter
+        glPushMatrix();
+          glTranslatef(0.7*radius,0.5*radius,-0.2*radius);
+          glScalef(0.3,0.3,2);
+          glColor3f(0,1,0);
+          glutSolidCube(1*radius);
+        glPopMatrix();
+
+        
+        //right shoulder Shooter
+        glPushMatrix();
+          glTranslatef(-0.7*radius,0.5*radius,-0.2*radius);
+          glScalef(0.3,0.3,2);
+          glColor3f(0,1,0);
+          glutSolidCube(1*radius);
+        glPopMatrix(); 
+        
       glPopMatrix();
-
-      //RED Material
-      m_amb[0] = 0.1745;
-      m_amb[1] = 0.01175;
-      m_amb[2] = 0.01175;
-      m_dif[0] = 0.61424;
-      m_dif[1] = 0.04316;
-      m_dif[2]= 0.04136;
-      m_spec[0] = 0.727811;
-      m_spec[1] = 0.626959;
-      m_spec[2]= 0.626959; 
-      shiny = 0.6 * 128;
-      glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, m_amb);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, m_dif);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, m_spec);
-      glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shiny);
-
-      //Center Shooting Part
-      glPushMatrix();
-        glTranslatef(0,0,1);
-        glColor3f(1,0,0);
-        glutSolidSphere(0.3,10,100);
-      glPopMatrix();
-      
-      //Obsidian Material
-      m_amb[0] = 0.05375;
-      m_amb[1] = 0.05;
-      m_amb[2] = 0.06625;
-      m_amb[3] = 0.82;
-      m_dif[0] = 0.18275;
-      m_dif[1] = 0.17;
-      m_dif[2]= 0.22525;
-      m_dif[3] = 0.82;
-      m_spec[0] = 0.332741;
-      m_spec[1] = 0.328634;
-      m_spec[2]= 0.508273; 
-      m_spec[3] = 0.346435;
-      shiny = 38.4;
-      glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, m_amb);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, m_dif);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, m_spec);
-      glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shiny);
-
-      //left shoulder Shooter
-      glPushMatrix();
-        glTranslatef(0.9,0.7,1);
-        glScalef(0.5,0.5,2);
-        glColor3f(0,1,0);
-        glutSolidCube(1);
-      glPopMatrix();
-
-      //right shoulder Shooter
-      glPushMatrix();
-        glTranslatef(-0.9,0.7,1);
-        glScalef(0.5,0.5,2);
-        glColor3f(0,1,0);
-        glutSolidCube(1);
-      glPopMatrix(); 
   
       //reset Material
       m_amb[0] = 0.1;
