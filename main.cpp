ed0926472839b4368b2405850b480ec9a929fe94
Cesar Santana <santanca@mcmaster.ca>
2016-12-09 14:26:28 -0500

    Added header comment

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/*
+Computer Graphics 3GC3 Final Project: Target Smash
+
+Cesar Antonio Santana Penner - 001411598
+Juan Carlos Santana Penner - 001411625
+Victor Timpau - 001414243
+Jin Lee - 001417622
+Date: December 9, 2016
+
+Description - On rails shooter game created with c++ and openGL. This is the main file
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
